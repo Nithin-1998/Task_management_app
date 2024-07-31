cff8156046f42a88a0b0d06957a3794a6b075304
Nithin <nithin230512@gmail.com>
2024-07-31 23:10:04 +0530

    Updated the methods in the settings file

--- task_management/settings.py
+++ task_management/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_ALLOWED_HOSTS',default = 'False', cast= bool)
+
+DEBUG = config('DJANGO_DEBUG',default = 'False', cast= bool)
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=Csv())
 
